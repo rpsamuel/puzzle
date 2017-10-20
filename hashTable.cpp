156f36bde3d4fbdd61a21ebb5b7a27136c54c8bd
rpsamuel <roderic.samuel@ebitscorp.com>
2017-10-19 21:53:06 -0400

    HashSize print

--- hashTable.cpp
+++ hashTable.cpp
@@ -12,7 +12,7 @@ hashTable::hashTable(int size){
     hashtable->resize(nSize);
     hashSize=nSize;
   }
-    
+  cout << "Hash SIZE: " << hashSize;  
   
   for(int i=0; i<hashSize; i++){
     list<string> p;
